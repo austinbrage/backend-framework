678baaeb8843990a1919affbf1739274a6efbb1f
agusb19 <agustinbrage19@gmail.com>
2024-03-11 18:18:27 -0300

    feat: change route folder to app folder directory
    (change watched architecture)

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -1,4 +1,4 @@
 import { join } from "path"
 
 export const serverFolder = join(process.cwd(), 'server')
-export const routeFolder = join(process.cwd(), 'app', 'routes')
+export const routeFolder = join(process.cwd(), 'app')
