5ad7bbddf0ad5ed58713bd68b2faee7097f87eeb
agusb19 <agustinbrage19@gmail.com>
2024-03-11 23:24:37 -0300

    feat: change output folder to server/routes folder directory
    (change output architecture)

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -1,4 +1,4 @@
 import { join } from "path"
 
-export const serverFolder = join(process.cwd(), 'server')
+export const serverFolder = join(process.cwd(), 'server', 'routes')
 export const routeFolder = join(process.cwd(), 'app')
