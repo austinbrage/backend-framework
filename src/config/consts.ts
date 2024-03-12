123a9784aba2aa005c93ba875b1b1c57d8fc77f6
agusb19 <agustinbrage19@gmail.com>
2024-03-11 23:56:38 -0300

    feat: apply new independent classes for routes and models
    (change input and output architecture)

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -1,4 +1,4 @@
 import { join } from "path"
 
 export const serverFolder = join(process.cwd(), 'server', 'routes')
-export const routeFolder = join(process.cwd(), 'app')
+export const appFolder = join(process.cwd(), 'app')
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,42 @@
 #!/usr/bin/env node
 
 import chokidar from 'chokidar'
-import { FolderOperations } from './classes/AppRoutes'
-import { routeFolder, serverFolder } from './config/consts'
+import { AppModel } from './classes/models/AppModel'
+import { AppRoute } from './classes/routes/AppRoute'
+import { appFolder, serverFolder } from './config/consts'
 
-const folderOperations = new FolderOperations({ routeFolder, serverFolder })
+const init = { appFolder, serverFolder }
 
-const watcher = chokidar.watch(routeFolder, {
-    ignored: /(^|[\/\\])\../, 
+const watcher = chokidar.watch(appFolder, {
+    ignored: /(^|[/\\])\../, 
     persistent: true
+})
+
+// Create, Delete Server Route
+watcher.on('addDir', async (path) => {
+    const appRoute = new AppRoute(init)
+    await appRoute.createServerFolder()
+    await appRoute.createRouteFolder(path)
+})
+
+watcher.on('unlinkDir', async (path) => {
+    const appRoute = new AppRoute(init)
+    await appRoute.deleteRouteFolder(path)
+})
+
+// Create Server Routes Model
+watcher.on('add', async (path) => {
+    const appModel = new AppModel(init)
+    await appModel.createTableFile(path)
+    await appModel.createQueryFieldFiles(path)
+})
+
+watcher.on('change', async (path) => {
+    const appModel = new AppModel(init)
+    await appModel.createTableFile(path)
+    await appModel.createQueryFieldFiles(path)
 })
 
 watcher.on('all', async (event, path) => { 
     console.log(event, path)
-    
-    await folderOperations.createServerFolder()
-
-    folderOperations.getRouteName()
-    folderOperations.getFilePath(path)
-
-    if(event === 'addDir') {
-        await folderOperations.createRouteFolder()
-    } else if(event === 'add' || event === 'change') {
-        await folderOperations.createTypesFile()
-        await folderOperations.createQueriesFile()
-    } else if(event === 'unlinkDir') {
-        await folderOperations.deleteRouteFolder()    
-    }
 })
